7aa1d5d5c61da1601e3127466ea485b2dd7fb037
ApichayaEmarat <67160040@kmitl.ac.th>
2025-11-06 01:41:49 +0700

    Update iszero.py

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,6 +1,5 @@
-user = input ("Please enter a number:")
-number = float(user)
-if number == 0:
+number = input ("Please enter a number:")
+if number == "0":
     print("This number is equal to zero.")
 else:
     print("This number is different from zero.")
